d0fb0fb2f2a649240e6545186d217aa63caa8fce
Marcel Legane <mlegane@gmail.com>
2020-02-04 00:07:11 +0000

    Update About copy

--- components/About/index.js
+++ components/About/index.js
@@ -5,67 +5,45 @@ const ThisAbout = () => (
   <About id="about">
     <Stack>
       <AboutTitle>About</AboutTitle>
+
       <AboutText>
-        In an era of laptop producers where everyone and their dog is an
-        engineer, Toby is a rare breed. Coming up in-house at Abbey Road - where
-        he spent 7 years locked in the studio and immersing himself in Abbey
-        Road’s rich recording history - he’s one of very few engineers to have
-        the chance to learn in such a professional studio environment, and to
-        develop working on a such a variety of styles and genres. He’s had the
-        chance to work with artists such as Nick Cave & The Bad Seeds, Oh
-        Wonder, Frank Ocean, Dirty Heads, Kasabian and Dawes. And being a part
-        of Abbey Road’s live sessions he had the opportunity to record Alabama
-        Shakes, Mumford & Sons, Izzy Bizu & Jack Garratt.
+        In an era of laptop producers that has led to everyone and their dog
+        calling themselves an engineer, Toby is a rare breed.
       </AboutText>
 
       <AboutText>
-        During his time at Abbey Road he worked with some of the world’s most
-        imminent producers - engineering for legends such as Tony Visconti, Alan
-        Parsons and Nile Rodgers to the younger generation Malay and Om’Mas
-        Keith. He also worked closely with many of the top Hollywood film
-        scoring engineers such as Peter Cobbin, Simon Rhodes, Dennis Sands and
-        Sam Okell.
+        After graduating from University of Surrey’s BMus Tonmeister course,
+        Toby made his way up the ranks in-house at Abbey Road where he spent
+        seven years immersing himself in the studio’s rich recording history and
+        working with some of the world’s most imminent producers including Tony
+        Visconti, Nile Rodgers and Malay. One of very few engineers to have the
+        opportunity to learn in such a professional studio environment, his
+        career developed by working on a huge variety of styles and genres. His
+        work includes recordings with artists such as Nick Cave & The Bad Seeds,
+        Oh Wonder, Frank Ocean, Dirty Heads, Kasabian and Dawes, and as part of
+        Abbey Road’s live sessions he’s worked with artists including Alabama
+        Shakes, Mumford & Sons, Izzy Bizu & Jack Garratt. Toby has also worked
+        as a score recordist on titles including the Oscar winning Gravity, The
+        Imitation Game, Suffragette and The Avengers, and as a digital scoring
+        engineer for Ridley Scott's The Martian, as well as engineering solo
+        sessions for Hacksaw Ridge.
       </AboutText>
 
       <AboutText>
-        Toby’s progression continued in 2019 when he moved to RCA Studio A -
-        Nashville, TN to work alongside six-time Grammy winning record producer
-        Dave Cobb, in search of the opportunity to make more records. Realising
-        that you never stop learning he’s now focused on continuing to learn
-        from the Southern American recording styles and whenever possible enjoys
-        creating natural recordings to tape with musicians playing live in the
-        room together.
+        In 2019 Toby moved to Nashville, TN, to work alongside six-time Grammy
+        winning record producer Dave Cobb at RCA Studio A. He’s now focused on
+        continuing to learn from the Southern American recording styles and
+        enjoys creating natural recordings to tape with musicians playing live
+        together.
       </AboutText>
 
       <AboutText>
-        Although his skills were developed in the digital era, Toby has studied
-        many analogue recording techniques while working across studios in
-        London and Nashville. He uses a hybrid approach, combining the best of
-        digital software with the character of analogue equipment, but adapts
-        his approach in order to service whatever the music. Toby brings a great
-        enthusiasm for music and experimentation to his sessions and aims to
-        create a relaxed atmosphere where artists can be creative.
-      </AboutText>
-
-      <AboutText>
-        Toby’s also gained great experience in soundtrack recording and
-        production, working as score recordist on titles such as the Oscar
-        winning Gravity, The Imitation Game, Suffragette (on which he was also
-        score editor), and The Avengers. During the summer of 2015 he enjoyed a
-        month working as digital scoring engineer for Ridley Scott's The
-        Martian, as well as engineering solo sessions for Hacksaw Ridge. He’s
-        recently recorded and mixed scores for Natalie Holt including the Royal
-        Television Society nominated score for BBC’s hard-hitting drama Three
-        Girls, BBC’s The Boy with the Topknot and the feature film Journey’s
-        End.
-      </AboutText>
-
-      <AboutText>
-        Recent notable projects include a 5.1 mix of a live concert for ‘Nick
-        Cave and the Bad Seeds’, a forthcoming record with LA-based ‘Dawes’,
-        ‘Dirty Heads’ new record ‘Supermoon’, sessions for the Monday Night
-        Football Theme at Blackbird and with Hank Williams Jr., a string session
-        for John Prine and the end credits song from the film ‘The Kitchen’.
+        Toby’s study of analogue recording techniques while working in studios
+        in London and Nashville means he uses a hybrid approach, combining the
+        best of digital software with the character of analogue equipment to
+        perfectly suit the music he records. A great enthusiasm for music and
+        experimentation means Toby’s sessions are in a relaxed atmosphere to
+        enable artists to be their most creative.
       </AboutText>
     </Stack>
   </About>
