64dcf47b3a3a8d1243a1755b9df845dd2d5c6e3b
Marcel Legane <mlegane@gmail.com>
2020-02-10 21:03:53 +0000

    Rebuild

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const isProd = (process.env.NODE_ENV || 'production') === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? '/tobyhulbert' : '/tobyhulbert',
+  assetPrefix: isProd ? '/tobyhulbert' : '',
   exportPathMap() {
     return {
       '/': { page: '/' },
