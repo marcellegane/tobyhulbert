c0435d30840aae192f63f409cf72e3c05c4cfd13
Marcel <marcel@toyfight.co>
2022-08-16 23:02:45 +0100

    Update bio

--- components/About/index.js
+++ components/About/index.js
@@ -8,43 +8,61 @@ const ThisAbout = () => (
       <AboutTitle>About</AboutTitle>
 
       <AboutText>
-        In an era of laptop producers that has led to everyone and their dog
-        calling themselves an engineer, Toby is a rare breed.
+        Toby is an engineer, mixer and producer currently based at Urchin
+        Studios, London.
       </AboutText>
 
       <AboutText>
-        After graduating from University of Surrey’s BMus Tonmeister course,
-        Toby made his way up the ranks in-house at Abbey Road where he spent
-        seven years immersing himself in the studio’s rich recording history and
-        working with some of the world’s most imminent producers including Tony
-        Visconti, Nile Rodgers and Malay. One of very few engineers to have the
-        opportunity to learn in such a professional studio environment, his
-        career developed by working on a huge variety of styles and genres. His
-        work includes recordings with artists such as Nick Cave & The Bad Seeds,
-        Oh Wonder, Frank Ocean, Dirty Heads, Kasabian and Dawes, and as part of
-        Abbey Road’s live sessions he’s worked with artists including Alabama
-        Shakes, Mumford & Sons, Izzy Bizu & Jack Garratt. Toby has also worked
-        as a score recordist on titles including the Oscar winning Gravity, The
-        Imitation Game, Suffragette and The Avengers, and as a digital scoring
-        engineer for Ridley Scott's The Martian, as well as engineering solo
-        sessions for Hacksaw Ridge.
+        After graduating from University of Surrey’s Tonmeister course, Toby
+        made his way up the ranks in-house at Abbey Road where he spent seven
+        years immersing himself in the studio’s rich recording history and
+        working with some of the world’s most imminent producers and engineers.
+        One of very few engineers to have the opportunity to learn in such a
+        professional studio environment, his career developed working on a huge
+        variety of styles and genres - from late night hiphop sessions to live
+        band tracking sessions in the Beatles room to classical and film score
+        recordings in the reverberant Studio 1. Toby’s burgeoning passion for
+        analogue recording developed through work in London and Nashville and
+        has led to a hybrid approach, combining the best of digital software
+        with the character of analogue equipment to best suit the music. A
+        life-long lover of music and experimentation - Toby’s sessions have a
+        relaxed atmosphere so to enable artists to be their most creative.
       </AboutText>
 
       <AboutText>
-        In 2019 Toby moved to Nashville, TN, to work alongside six-time Grammy
-        winning record producer Dave Cobb at RCA Studio A. He’s now focused on
-        continuing to learn from the Southern American recording styles and
-        enjoys creating natural recordings to tape with musicians playing live
-        together.
+        In 2019, Toby left Abbey Road and followed the music when he was offered
+        the chance to move to Nashville to work as engineer for multi-Grammy
+        winning record producer Dave Cobb at Historic RCA Studio A. During an
+        inspiring time in Nashville he engineered many memorable sessions with
+        Cobb including work with Barry Gibb, Dawes, The Highwomen, Jason Isbell,
+        Dolly Parton, Brandi Carlile, Chris Stapleton, Dirty Heads, Keith Urban
+        and Anderson East. Much of the time at RCA Studio A was recording bands
+        playing together live, with Dave Cobb often producing from the studio
+        floor, recording to tape - more akin to a 60s and 70s recording style
+        not so often seen these days.
       </AboutText>
 
       <AboutText>
-        Toby’s study of analogue recording techniques while working in studios
-        in London and Nashville means he uses a hybrid approach, combining the
-        best of digital software with the character of analogue equipment to
-        perfectly suit the music he records. A great enthusiasm for music and
-        experimentation means Toby’s sessions are in a relaxed atmosphere to
-        enable artists to be their most creative.
+        His varied music work at Abbey Road included sessions with artists such
+        as Nick Cave & The Bad Seeds, Frank Ocean, Kasabian, and as part of
+        Abbey Road’s live sessions he worked with artists including Alabama
+        Shakes and Mumford & Sons.
+      </AboutText>
+
+      <AboutText>
+        Toby also has extensive film scoring experience, initially as a score
+        recordist on titles such as the Oscar winning Gravity, The Imitation
+        Game, Suffragette, The Avengers and The Martian and more recently as
+        scoring engineer and score mixer. Recent TV and film work includes
+        engineering on Baz Luhrmann’s Elvis film and mixing for UK based
+        composers Paul Englishby and Carly Paradis.
+      </AboutText>
+
+      <AboutText>
+        Toby now works out of his own room at Urchin Studios, London as a
+        freelance engineer, mixer and producer on music and film productions.
+        With a window through to the Urchin live space - Toby is setup to record
+        band and overdub sessions directly from his control room.
       </AboutText>
     </Stack>
   </HomeSection>
